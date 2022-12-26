899f2e2f8daf456c49b2943ac214c6a9ce873875
sk0402 <sanathachar007@gmail.com>
2022-12-26 11:26:56 +0530

    changes

--- day 01/index.js
+++ day 01/index.js
@@ -1,3 +1,11 @@
+
+
+const heading1 = React.createElement("h1", { id: "head1" }, "Namaste to heading1");
+const heading2 = React.createElement("h2", { id: "head2" }, "namaste to hrading2");
+const root = ReactDOM.createRoot(document.getElementById("root"));
+const container=React.createElement("div",{id:"content"}, [heading1,heading2]);
+root.render(container);
+
 const heading = React.createElement("h1", { id: "title" }, "Namaste React using React");
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root_react = ReactDOM.createRoot(document.getElementById('root_react'));
 root.render(heading);
